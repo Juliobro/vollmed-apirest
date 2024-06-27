bdf86a0dde06858dc00800ecd69ac35147af8d1c
Julian Munera <julian.564915@gmail.com>
2024-06-26 23:36:48 -0500

    Adding migrations to insert rows into the "medicos" and "pacientes" tables for easy API testing.

---
+++ src/main/resources/db/migration/V8__insert-into-table-pacientes.sql
@@ -0,0 +1,12 @@
+INSERT INTO `vollmed_api`.`pacientes` (`nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`)
+VALUES ('Julian Munera', 'j.munera@mail.com', '128493', '32', '5', 'A', '45', 'Medellin', '3192837465');
+INSERT INTO `vollmed_api`.`pacientes` (`nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`)
+VALUES ('Maria Garcia', 'm.garcia@mail.com', '837291', '47', '3', 'B', '62', 'Bogota', '3173648291');
+INSERT INTO `vollmed_api`.`pacientes` (`nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`)
+VALUES ('Carlos Perez', 'c.perez@mail.com', '748291', '29', '8', 'C', '71', 'Cali', '3147593821');
+INSERT INTO `vollmed_api`.`pacientes` (`nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`)
+VALUES ('Ana Rodriguez', 'a.rodriguez@mail.com', '582739', '58', '2', 'D', '89', 'Barranquilla', '3164859201');
+INSERT INTO `vollmed_api`.`pacientes` (`nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`)
+VALUES ('Luis Martinez', 'l.martinez@mail.com', '394857', '43', '9', 'E', '34', 'Cartagena', '3157482930');
+INSERT INTO `vollmed_api`.`pacientes` (`nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`)
+VALUES ('Sofia Lopez', 's.lopez@mail.com', '291748', '36', '1', 'F', '52', 'Bucaramanga', '3119283746');
---
+++ src/main/resources/db/migration/V9__insert-into-table-medicos.sql
@@ -0,0 +1,12 @@
+INSERT INTO `vollmed_api`.`medicos` (`nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('Antonio Perez', 'a.perez@mail.com', '121212', 'ORTOPEDIA', '12', '3', 'A', '45', 'Bogotá', '3132847582', '1');
+INSERT INTO `vollmed_api`.`medicos` (`nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('Bernardo Lopez', 'b.lopez@mail.com', '232323', 'ORTOPEDIA', '23', '5', 'B', '52', 'Medellín', '3148572930', '1');
+INSERT INTO `vollmed_api`.`medicos` (`nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('Carlos Ramirez', 'c.ramirez@mail.com', '343434', 'CARDIOLOGIA', '34', '7', 'C', '62', 'Cali', '3159732845', '1');
+INSERT INTO `vollmed_api`.`medicos` (`nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('Diego Martinez', 'd.martinez@mail.com', '454545', 'CARDIOLOGIA', '45', '2', 'D', '71', 'Barranquilla', '3164857291', '1');
+INSERT INTO `vollmed_api`.`medicos` (`nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('Eduardo Garcia', 'e.garcia@mail.com', '565656', 'GINECOLOGIA', '56', '9', 'E', '89', 'Cartagena', '3175932847', '1');
+INSERT INTO `vollmed_api`.`medicos` (`nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('Fernando Alvarez', 'f.alvarez@mail.com', '676767', 'PEDIATRIA', '67', '8', 'F', '34', 'Bucaramanga', '3184729103', '1');
